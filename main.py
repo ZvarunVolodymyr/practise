25e25076953a3c2e7509dc6848c7f1525a5ecb0a
lolpie24 <gasf323@mail.ru>
2021-09-10 15:38:15 +0300

    task 1

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-print('Введіть 2 натуральні числа через пробіл')
-flag = True
-while(flag):
-    try:
-        n, m = map(int, input().split())
-        if n <= 0 or m <= 0:
-            int('error')
-        flag = False
-    except ValueError:
-        print('Неправильний ввід, спробуйте йще раз')
-        continue
+def main_function():
+    print('Введіть 2 натуральні числа через пробіл')
+    flag = True
+    while(flag):
+        try:
+            n, m = map(int, input().split())
+            if n <= 0 or m <= 0:
+                int('error')
+            flag = False
+        except ValueError:
+            print('Неправильний ввід, спробуйте йще раз')
+            continue
 
-if n > m:
-    n, m = m, n
+    if n > m:
+        n, m = m, n
 
-if n == 1:
-    print(m * 4)
-    exit()
+    if n == 1:
+        print(m * 4)
+        exit()
 
-answer = 2 * (n * (m + 1) + m)
-additional = 0
+    answer = 2 * (n * (m + 1) + m)
+    additional = 0
 
-if n % 2 != 0:
-    additional += m
+    if n % 2 != 0:
+        additional += m
 
-if m % 2 != 0:
-    additional += n
+    if m % 2 != 0:
+        additional += n
 
-if additional != 0:
-    additional -= 2
+    if additional != 0:
+        additional -= 2
 
-answer += additional
+    answer += additional
 
-print(answer)
+    return(answer)
+
+print(main_function())
