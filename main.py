bc59bea8c503c3ebe173819df9bbc220777ba215
lolpie24 <gasf323@mail.ru>
2021-09-16 22:39:48 +0300

    task 2

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
-def main_function(n: int, m: int):
-    if n > m:
-        n, m = m, n
+# right->down->left->up == 0->1->2->3
+class spiral_trail:
+    i = 0
+    j = 0
+    n = 0
+    matrix = []
+    direction = 0
 
-    if n == 1:
-        return m * 4
+    def __init__(self, n:int):
+        self.matrix = [[0] * n for i in range(n)]
+        self.n = n
 
-    answer = 2 * (n * (m + 1) + m)
-    additional = 0
+    def direction_change(self):
+        i = self.i
+        j = self.j
+        if self.direction in (0, 2):
+            j += (1 if self.direction == 0 else -1)
+        else:
+            i += (1 if self.direction == 1 else -1)
 
-    additional += (m if n % 2 != 0 else 0) + (n if m % 2 != 0 else 0)
-    additional -= 2 if additional != 0 else 0
+        if not (0 <= i < self.n and 0 <= j < self.n and self.matrix[i][j] == 0):
+            self.direction = (self.direction + 1) % 4
 
-    answer += additional
-    return(answer)
+    def next_step(self):
+        if self.direction in (0, 2):
+            self.j += (1 if self.direction == 0 else -1)
+        else:
+            self.i += (1 if self.direction == 1 else -1)
+
+    def set_value(self, val:int):
+        self.matrix[self.i][self.j] = val
 
 
-def input_int_number(name=''):
-    print(f"Введіть натуральне число {name}(без зайвих символів)")
+
+def main_function(n:int):
+    spiral = spiral_trail(n)
+    for i in range(1, n**2 + 1):
+        spiral.set_value(i)
+        spiral.direction_change()
+        spiral.next_step()
+
+    return spiral.matrix
+
+def input_natural_number():
     while True:
         try:
-            new_nam = int(input())
-            if new_nam <= 0:
+            n = int(input())
+            if n <= 0:
                 int('error')
-            return new_nam
+            return n
         except ValueError:
-            print('Неправильний ввід, спробуйте йще раз')
-            continue
+            print('Ввід не правильний, спробуйте йще раз')
 
 
-def cin():
-    return [input_int_number('n'), input_int_number('m')]
+def output(n:int, matrix):
+    for i in range(n):
+        print(matrix[i])
 
 
-input_value = cin()
-print(main_function(input_value[0], input_value[1]))
+n = input_natural_number()
+output(n, main_function(n))
