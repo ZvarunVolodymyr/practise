1b8a326b17ac588e187c278d9cfc0c37de2dd900
lolpie24 <gasf323@mail.ru>
2021-09-25 21:52:57 +0300

    task 3

--- main.py
+++ main.py
@@ -1,98 +1,5 @@
 import validation
-
-
-class Node:
-    def __init__(self, previous=None, next=None, val=None):
-        self.previous = previous
-        self.next = next
-        self.val = val
-
-
-class ListIter:
-    def __init__(self, node: Node):
-        self.node = node
-
-    def __next__(self):
-        if self.node is None:
-            raise StopIteration
-        value = self.node.val
-        self.node = self.node.next
-        return value
-
-
-class LinkedList:
-    start: Node = None
-    end: Node = None
-    length = 0
-
-    def push_back(self, new_val):
-        self.length += 1
-        if self.start is None:
-            self.start = self.end = Node(val=new_val)
-            return
-        self.end.next = Node(val=new_val, previous=self.end)
-        self.end = self.end.next
-
-    def push_front(self, new_val):
-        self.length += 1
-        if self.start is None:
-            self.start = self.end = Node(val=new_val)
-            return
-        self.start.previous = Node(val=new_val, next=self.start)
-        self.start = self.start.previous
-
-    def __getitem__(self, item):
-        if item >= self.length:
-            raise IndexError
-
-        for i, val in enumerate(self):
-            if i == item:
-                return val
-
-    def __setitem__(self, key, value):
-        if key >= self.length:
-            raise IndexError
-        this = self.start
-        for i in range(self.length):
-            if i == key:
-                this.val = value
-                return
-            this = this.next
-
-
-
-    def __str__(self):
-        s = '['
-        this = self.start
-        for val in self:
-            s += str(val) + ', '
-        s = s[:-2] + ']'
-        return s
-
-    def __repr__(self):
-        return str(self)
-
-    def __len__(self):
-        return self.length
-
-    def __iter__(self):
-        return ListIter(self.start)
-
-    def max_element(self):
-        if self.start is None:
-            return None
-        max_ = self.start.val
-        for i in self:
-            max_ = max(i, max_)
-        return max_
-
-    def min_element(self):
-        if self.start is None:
-            return None
-        min_ = self.start.val
-        for i in self:
-            min_ = min(i, min_)
-        return min_
+from LinkedList import LinkedList
 
 
 def main_function(n, elements_list: LinkedList):
@@ -112,15 +19,13 @@ def main_function(n, elements_list: LinkedList):
 
 
 def cin():
-    list_ = LinkedList()
     n = int(validation.input_validation('Введіть розмір масиву', validation.is_natural_number))
-    print(f'Введіть {n} елементів, кожен елемент в новому рядку')
-    for i in range(n):
-        list_.push_back(float(validation.input_validation(additional_condition=validation.is_float_number)))
-
+    list_ = LinkedList()
+    list_.input(n, f'Введіть {n} елемента, кожен елемент в одному рядку', float, validation.is_float_number)
     return list_
 
 
 list_ = cin()
-n = len(list_)
-print(main_function(n, list_))
+list_ = main_function(len(list_), list_)
+print(list_)
+
--- validation.py
+++ validation.py
@@ -33,13 +33,29 @@ def is_greater_then(n:str, value:[]):
     return True
 
 
+def is_valid_array(n: str, conditional: []):
+    if conditional[0] is None:
+        return True
+    s = []
+    if conditional[1] == -1:
+        s = n.split()
+    else:
+        s = n.split()[:conditional[1]]
+        if len(s) < conditional[1]:
+            return False
+    for i in s:
+        if not conditional[0](i):
+            return False
+    return True
+
+
 def is_valid(n: str, additional_condition=None, value_for_conditional=None):
     if additional_condition is None:
         return True
     return additional_condition(n, value_for_conditional)
 
 
-def input_validation(text="", additional_condition=None, *value_for_conditional, is_strip = True):
+def input_validation(text="", additional_condition=None, is_strip=True, *value_for_conditional):
     slesh_n = '\n'
     print(text, end=f'{slesh_n if text != "" else ""}')
     while True:
@@ -55,3 +71,14 @@ def input_validation(text="", additional_condition=None, *value_for_conditional,
         except KeyboardInterrupt:
             print('Програма завершила свою роботу')
             exit()
+
+
+def array_input(text="", size=-1, additional_condition=None, type=str, split_symbol=' ', is_strip=True):
+    func = type
+    if is_strip:
+        func = lambda x: type(x.strip())
+    list_ = input_validation(text, is_valid_array, is_strip, additional_condition, size).split()
+    if size != -1:
+        list_ = list_[:size]
+    list_ = list(map(func, list_))
+    return list_
