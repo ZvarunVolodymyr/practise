4599c281df8cd0dc094bd9bced7af8169d5ceee6
lolpie24 <gasf323@mail.ru>
2021-09-26 15:09:22 +0300

    task 3

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ from LinkedList import LinkedList
 
 
 def cin():
-    n = int(validation.input_validation('Введіть розмір масиву', validation.is_natural_number))
-    list_ = LinkedList()
-    list_.input(f'Введіть {n} елемента, кожен елемент в одному рядку', n, float, validation.is_float_number)
-    return list_
+    n = validation.input_validation('Ведіть(без лапок):\n'
+                                    '"1" - ввести масив\n'
+                                    '"2" - згенерувати масив\n'
+                                    '"exit" - вихід з програми',
+                                    validation.is_menu)
+    if n == 'exit':
+        print('програма завершила свою роботу')
+        exit()
+    n = int(n)
+    m = int(validation.input_validation('Введіть ціле число - розмір масиву', validation.is_natural_number))
+    if n == 1:
+        list_ = LinkedList()
+        list_.input(f'Введіть {m} елемента, кожен елемент в одному рядку', m, float, validation.is_float_number)
+        # також можна записати так
+        # list_ = LinkedList()
+        # list_.input('Введіть масив', type=float, additional_condition=validation.is_float_number, input_size=True)
+        return list_
+    if n == 2:
+        left = int(validation.input_validation('Введіть ліву межу генерації(ціле число)', validation.is_int_number))
+        right = int(validation.input_validation('Введіть праву межу генерації(ціле число)', validation.is_greater_then, left))
+        list_ = LinkedList()
+        list_.generate(m, left, right)
+        print('Згенерований масив :', list_)
+        return list_
 
 
-list_ = cin()
-
-# також можна записати так
-# list_ = LinkedList()
-# list_.input('Введіть масив', type=float, additional_condition=validation.is_float_number, input_size=True)
-list_.get_answer_for_task()
-print(list_)
+while True:
+    list_ = cin()
+    list_.get_answer_for_task()
+    print(list_)
 
