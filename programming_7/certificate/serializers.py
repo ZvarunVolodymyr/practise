31a80727d7aa97720112a04a732c38642e0ae460
ZvarunVolodymyr <90132347+ZvarunVolodymyr@users.noreply.github.com>
2021-11-16 23:34:36 +0200

    Add files via upload

--- programming_7/certificate/serializers.py
+++ programming_7/certificate/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from certificate.models import Certificate
 from certificate.validation import vaccine_validation
 import functools
-
+from validation import validator_
 
 class CertificateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,19 +10,4 @@ class CertificateSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'birth_date', 'start_date', 'end_date', 'international_passport', 'vaccine')
 
     def validator(self, data):
-        is_valid_ = functools.partial(vaccine_validation, data=data)
-        errors = {}
-        self.is_valid()
-
-        for i in self.errors.items():
-            errors[i[0]] = i[1][0]
-
-        for i in data.keys():
-            try:
-                data[i] = is_valid_(i)
-            except Exception as error:
-                data[i] = None
-                errors[i] = error
-        if len(errors) != 0:
-            raise serializers.ValidationError(errors)
-        return data
+        return validator_(self, data, vaccine_validation)
